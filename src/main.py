fc415e11bb70d94949265511ee75b64df5146a8a
TahaHachana <tahahachana@gmail.com>
2024-06-01 15:36:33 +0100

    Add child HTML template with Bootstrap carousel

--- src/main.py
+++ src/main.py
@@ -34,17 +34,20 @@ def rec_list_folders_in_dir(dir):
 def move_up_one_dir(path):
     new_path = path.replace(CONTENT_DIR, ".")
     if new_path != ".":
-        #print(f"deleting {new_path}")
+        # print(f"deleting {new_path}")
         shutil.rmtree(new_path, ignore_errors=True)
     # if not os.path.exists(new_path):
     os.makedirs(new_path, exist_ok=True)
     print(f"Created directory: {new_path}")
     return new_path
 
+
 folders_to_exclude = ["content", ".git", "src"]
+
 
 def not_excluded_folder(folder):
     return folder not in folders_to_exclude
+
 
 def write_index_html(new_path):
     folder_names = [
@@ -53,9 +56,7 @@ def write_index_html(new_path):
         if os.path.isdir(os.path.join(new_path, name))
     ]
     folder_names = list(filter(not_excluded_folder, folder_names))
-    links = [
-            {"href": f"./{folder}/", "text": folder} for folder in folder_names
-        ]
+    links = [{"href": f"./{folder}/", "text": folder} for folder in folder_names]
     context = {
         "title": "My Title",
         "links": links,
@@ -65,6 +66,13 @@ def write_index_html(new_path):
     with open(index_file_path, "w") as index_file:
         index_file.write(html_output)
         print(f"Created index.html in: {new_path}")
+
+
+def contains_md_files(folder):
+    for file in os.listdir(folder):
+        if file.endswith(".md"):
+            return True
+    return False
 
 
 def main():
@@ -82,3 +90,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Todo: Carousel child HTML template
+# Todo: Markdown to HTML conversion
+# Todo: Generate carousel page from markdown files
